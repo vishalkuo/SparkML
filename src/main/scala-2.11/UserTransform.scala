07eb13ac3fffaedee4fb94945a97bbd184b0bda5
Vishal Kuo <vishalkuo@hotmail.com>
2016-01-27 15:45:32 -0500

    User agg/transforms complete

--- src/main/scala-2.11/UserTransform.scala
+++ src/main/scala-2.11/UserTransform.scala
@@ -11,8 +11,16 @@ class UserTransform(sc: SparkContext, rawFields: RDD[String]) {
   val declaredOccupations = userFields.map(fields => fields(3)).distinct().count()
   val numOfZipcodes = userFields.map(fields => fields(4)).distinct().count()
   val allAges = userFields.map(fields => fields(1).toInt)
+  val occupationsIndexed = userFields.map(fields => fields(3)).distinct().collect()
+  val occupationMap = occupationsIndexed.zipWithIndex.toMap
 
-  def ageDistribution(): RDD[(Int, Int)] = {
+
+  def getAgeDistribution(): RDD[(Int, Int)] = {
     userFields.map(fields => (fields(1).toInt, 1)).reduceByKey((a, b) => a + b).sortBy(_._1)
   }
+
+  def getOccupationDistribution(): RDD[(String, Int)] = {
+    userFields.map(fields => (fields(3), 1)).reduceByKey(_ + _).sortBy(_._2)
+  }
+  
 }
