37e65252da9efc659bd206097a901cebc3948d9c
Vishal Kuo <vfkuo@uwaterloo.ca>
2016-01-27 23:01:40 -0500

    Serialize movie agg and perform movie transfo

--- src/main/scala-2.11/MovieTransform.scala
+++ src/main/scala-2.11/MovieTransform.scala
@@ -1,10 +1,9 @@
-import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
 /**
   * Created by vishalkuo on 2016-01-27.
   */
-class MovieTransform(sc: SparkContext, rawFields: RDD[String]) {
+class MovieTransform(rawFields: RDD[String]) extends Serializable{
   val movieFields = rawFields.map(lines => lines.split("\\|"))
   val years = movieFields.map(fields => fields(2)).map({
     field => try{
@@ -16,17 +15,14 @@ class MovieTransform(sc: SparkContext, rawFields: RDD[String]) {
 
   val yearsFiltered = years.filter(_ != 1900)
   val yearsMean = yearsFiltered.mean()
-//
-//  def getYearsFilled: RDD[Int] = {
-//    years.map({
-//      case 1900 => yearsMean.toInt
-//      case x => x.toInt
-//    })
-//  }
-//
-//
-//
-//  def getAgeAggregated: Array[(Int, Long)] = {
-//    getYearsFilled.map(x => 1998 - x).countByValue().toArray.sortBy(_._1)
-//  }
+
+  val yearsFilled = years.map(x => if (x == 1900) yearsMean else x)
+
+
+
+
+
+  def getAgeAggregated: Array[(Double, Long)] = {
+    yearsFilled.map(x => 1998 - x).countByValue().toArray.sortBy(_._1)
+  }
 }
--- src/main/scala-2.11/UserTransform.scala
+++ src/main/scala-2.11/UserTransform.scala
@@ -4,7 +4,7 @@ import org.apache.spark.rdd.RDD
 /**
   * Created by vishalkuo on 2016-01-26.
   */
-class UserTransform(sc: SparkContext, rawFields: RDD[String]) {
+class UserTransform(rawFields: RDD[String]) {
   val userFields = rawFields.map(line => line.split("\\|"))
   val totalUsers = userFields.count()
   val genders = userFields.map(fields => fields(2)).distinct()
