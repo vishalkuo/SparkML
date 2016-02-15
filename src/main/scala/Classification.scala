674ca5f6330a96d4e4d628b5648a9f767485052a
Vishal Kuo <vfkuo@uwaterloo.ca>
2016-02-14 22:52:14 -0500

    Add acc's for classifier models

--- src/main/scala/Classification.scala
+++ src/main/scala/Classification.scala
@@ -1,6 +1,12 @@
+import org.apache.spark.mllib.classification.{NaiveBayes, SVMWithSGD, LogisticRegressionWithSGD}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.configuration.Algo
+import org.apache.spark.mllib.tree.impurity.Entropy
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
+
 
 /**
   * Created by vishalkuo on 2016-02-13.
@@ -8,6 +14,8 @@ import org.apache.spark.{SparkContext, SparkConf}
 object Classification {
   val conf = new SparkConf().setMaster("local[2]")
     .setAppName("StumbleUpon Classifier").set("spark.executor.memory", "2g")
+  val iterations = 10
+  val treeDepth = 5
   val sc = new SparkContext(conf)
   def main(args: Array[String]) {
     val rawData = sc.textFile("src/main/resources/datasets/StumbleUpon/train.tsv")
@@ -27,7 +35,38 @@ object Classification {
       LabeledPoint(label, Vectors.dense(feature))
     }
     mllibData.cache()
-    println(mllibData.count())
-    println(naiveBayesData.count())
   }
+
+  def logisticRegression(data: RDD[LabeledPoint])= {
+    val model = LogisticRegressionWithSGD.train(data,iterations)
+    val correctCount = data.map(lPoint =>
+    if (model.predict(lPoint.features) == lPoint.label) 1 else 0).sum
+    val acc = correctCount / data.count()
+    println(acc)
+  }
+
+  def svm(data: RDD[LabeledPoint]) = {
+    val model = SVMWithSGD.train(data, iterations)
+    val correctCount = data.map(lPoint =>
+      if (model.predict(lPoint.features) == lPoint.label) 1 else 0).sum
+    val acc = correctCount / data.count()
+    println(acc)
+  }
+
+  def naiveBayes(data: RDD[LabeledPoint]) = {
+    val model = NaiveBayes.train(data)
+    val correctCount = data.map(lPoint =>
+      if (model.predict(lPoint.features) == lPoint.label) 1 else 0).sum
+    val acc = correctCount / data.count()
+    println(acc)
+  }
+
+  def decisionTree(data: RDD[LabeledPoint]) = {
+    val model = DecisionTree.train(data, Algo.Classification, Entropy, treeDepth)
+    val correctCount = data.map(lPoint =>
+      if (model.predict(lPoint.features) == lPoint.label) 1 else 0).sum
+    val acc = correctCount / data.count()
+    println(acc)
+  }
+
 }
